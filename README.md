8ce50946503d8dfb5b3fdc022b79642d3a889219
Christopher <mrcm8263@gmail.com>
2020-06-27 16:56:46 +1000

    Initial commit
    DO NOT BLINDLY UPDATE // THIS IS A DEVELOPMENT BUILD
    
    Important Note: If you test this resource in your server, please report back to us! We have not thoroughly tested this update and would appreciate your feedback.
    
    See readme for features and instructions.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Vehicle-Attachment
-A lightweight vehicle attachment/tow script for FiveM.
+![channels4_banner|690x114](https://i.ibb.co/CHMD8y6/channels4-banner.jpg) 
+# Inferno Collection: Vehicle Attachment
+[![Build Status](https://travis-ci.com/inferno-collection/Vehicle-Attachment.svg?branch=development)](https://travis-ci.com/inferno-collection/TIC)
+
+__Public Alpha Version 1.0__
+
+A lightweight vehicle attachment/tow script for FiveM. Attach vehicles to tow trucks and adjust the positioning with easy to use controls.
+
+Presently, the following can be customized:
+- Vehicles that can be used to tow other vehicles
+- Default movement speed of vehicle being attached  
+
+Usage:
+- Go up to a tow vehicle and type `/attachment`
+- Select the tow vehicle and vehicle to be towed with the Enter key
+- Follow on screen instructions
+- When ready to detach, type `/attachment` again
+
+***
+### Pictures
+![Image 1](https://i.ibb.co/VxRNcxd/image.png)
+
+> The Inferno Collection Team
+* ChristopherM
+* Scott_UK 
+* FrozenN00b
+***
+Like what you see? [Check out our other resource](https://inferno-collection.com/resources).
---
+++ [inferno-collection]/inferno-vehicle-attachment/fxmanifest.lua
@@ -0,0 +1,25 @@
+-- Inferno Collection Vehicle Attachment 1.0 Alpha
+--
+-- Copyright (c) 2020, Christopher M, Inferno Collection. All rights reserved.
+--
+-- This project is licensed under the following:
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to use, copy, modify, and merge the software, under the following conditions:
+-- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. THE SOFTWARE MAY NOT BE SOLD.
+--
+
+name "Vehicle Attachment - Inferno Collection"
+
+description "A lightweight vehicle attachment/tow script for FiveM."
+
+author "Inferno Collection (inferno-collection.com)"
+
+version "1.0 Alpha"
+
+url "https://inferno-collection.com"
+
+client_script "client.lua"
+
+fx_version "bodacious"
+
+game "gta5"
