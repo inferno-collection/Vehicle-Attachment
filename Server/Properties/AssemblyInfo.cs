e70d5f50db2a96c3de0946cff2f29a3ff47e5c9f
Christopher <mrcm8263@gmail.com>
2021-05-03 22:16:16 +1000

    Updated package references

--- Server/Properties/AssemblyInfo.cs
+++ Server/Properties/AssemblyInfo.cs
@@ -2,9 +2,9 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyCopyright("Copyright (c) 2019-2020, Christopher M, Inferno Collection. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright (c) 2019-2021, Christopher M, Inferno Collection. All rights reserved.")]
 [assembly: Guid("57a291f3-4428-4c22-bb95-638952005ef7")]
 [assembly: AssemblyCompany("Inferno Collection")]
 [assembly: NeutralResourcesLanguage("en-US")]
-[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
 [assembly: ComVisible(false)]
